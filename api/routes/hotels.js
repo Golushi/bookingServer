79ded2c95aec82532f1f053882ced9504e3c82af
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2023-06-21 14:26:12 +0200

    little update

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,4 @@
 import express from "express";
-import Hotel from "../models/Hotel.js";
-import { createError } from "../utils/error.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 import {
   createHotel,
